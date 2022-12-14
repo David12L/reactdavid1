2f3d72c668364b6ade01a117d527e7f1487144ed
luis2189p <luis2189p@gmail.com>
2022-12-14 11:17:01 -0500

    react

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import { collection, getDocs, query, doc, deleteDoc, where, onSnapshot} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "./componente/firebase";
+import AppForm from "./componente/AppForm";
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function App(){
+  //////////////////READ///////////////////
+  const [idActual, setIdActual] = useState("");
+  const [docsBD, setDocsBD] = useState([]);
+  const [orden, setOrden] = useState ([]);
+  const i = 1;
+
+  /////////////LECTURA A BD///////////////
+  useEffect( () => {
+    const xCollectionConQuery =  query(collection(db, "persona"), where ("nombre", "!=", ""));
+    const unsubscribe = onSnapshot(xCollectionConQuery, (xDatosBD)=>{
+      const xDoc  = [];
+      xDatosBD.forEach((doc) => {
+        xDoc.push({id: doc.id, ...doc.data()})
+      });
+      
+      setDocsBD(xDoc); 
+    });
+  }, [idActual]);
+ 
+  const fnDelete = async (xId) =>{
+    if(window.confirm("deberdad desea borrar")){
+      await deleteDoc(doc(db, 'persona', xId));
+      toast("Documento ELIMINADO con exito",{
+        type:'error',
+        autoClose: 2000
+      })
+    }
+  }
+  return( 
+    <div className="container text-center">
+      <div className="card bs-secondary p-3 mt-3">
+        <ToastContainer/>
+        
+        <div className="col-md-12 p-2">
+          <div className="card mb-1">
+            <h1>React David</h1>
+          </div>
+        </div>
+
+        <div className="col-md-12 p-2">
+          <AppForm {...{idActual,setIdActual}}/>
+        </div>
+
+        <div className="col-md-12 p-2">
+          {
+            docsBD.map((p) =>
+              <div className="car mb-1" key={p.id}>
+                <div className="d-flex justify-content-between">
+                  <h4>N     -{p.nombre}</h4>
+                  <div>
+                    <i className="material-icons text-danger"
+                      onClick={() => fnDelete(p.id)}>Borrar</i>
+                      ///
+                    <i className="material-icons text-warnig"
+                      onClick={() => setIdActual(p.id)}>Modifica</i>  
+                  </div>
+                </div>
+                <div className="d-flex justify-content">
+                  <span>Edad: {p.edad}</span>...
+                  <a href="#">Genero:{p.genero}</a>
+                </div>
+              </div>
+              )
+          }
+        </div>
+
+      </div> 
+    </div>
+  );
+};
+
+
+export default App;
